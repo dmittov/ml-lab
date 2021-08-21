74fee009b7e8ef15efcf9044bd7ba76fe25b5eaa
Dmitry Mittov <mittov@gmail.com>
2021-08-21 21:38:50 +0200

    K8s Spark Operator

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -18,6 +18,18 @@ locals {
   cluster_zone = "${data.google_client_config.default.region}-${var.zone}"
 }
 
+resource "google_service_account" "k8s_spark" {
+  account_id   = "k8s-spark-service-accout"
+  display_name = "Service Account for K8s/Spark"
+}
+
+resource "google_project_iam_binding" "k8s_spark" {
+    role    = "roles/storage.admin"
+    members = [
+      "serviceAccount:${google_service_account.k8s_spark.email}"
+    ]
+}
+
 resource "google_container_cluster" "kube" {
   name = "dl-cluster"
   # Zonal cluster is good enough and GKE fee is $0
@@ -52,14 +64,14 @@ resource "google_container_cluster" "kube" {
 
 # additional node pool example
 resource "google_container_node_pool" "additional_preemptible_nodes" {
-  count = 0
+  count = 1
   name       = "additional-node-pool"
   location   = local.cluster_zone
   cluster    = google_container_cluster.kube.name
-  node_count = 1
+  node_count = 2
   node_config {
     preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = "n2d-standard-4"
 
     service_account = data.terraform_remote_state.persistent.outputs.service_account.email
     oauth_scopes = [
@@ -71,5 +83,59 @@ resource "google_container_node_pool" "additional_preemptible_nodes" {
     update = "10m"
     delete = "10m"
   }
+}
 
+provider kubernetes {
+    host                   = "${google_container_cluster.kube.endpoint}"
+    token                  = "${data.google_client_config.default.access_token}"
+    client_certificate     = "${base64decode(google_container_cluster.kube.master_auth.0.client_certificate)}"
+    client_key             = "${base64decode(google_container_cluster.kube.master_auth.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(google_container_cluster.kube.master_auth.0.cluster_ca_certificate)}"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "${google_container_cluster.kube.endpoint}"
+    token                  = "${data.google_client_config.default.access_token}"
+    client_certificate     = "${base64decode(google_container_cluster.kube.master_auth.0.client_certificate)}"
+    client_key             = "${base64decode(google_container_cluster.kube.master_auth.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(google_container_cluster.kube.master_auth.0.cluster_ca_certificate)}"
+  }
+}
+
+resource "kubernetes_namespace" "spark_jobs" {
+  metadata {
+    name = "spark-jobs"
+  }
+}
+
+resource "helm_release" "k8s-spark" {
+  name       = "spark-operator"
+  repository = "https://googlecloudplatform.github.io/spark-on-k8s-operator"
+  chart      = "spark-operator"
+  namespace  = "spark-operator"
+  create_namespace = true
+  set {
+    name = "sparkJobNamespace"
+    value = kubernetes_namespace.spark_jobs.metadata[0].name
+    type = "string"
+  }
+}
+
+resource "google_service_account_key" "k8s_spark" {
+  service_account_id = google_service_account.k8s_spark.name
+}
+
+resource "kubernetes_secret" "google-application-credentials" {
+  metadata {
+    name = "k8s-spark-secret"
+    namespace = "spark-jobs"
+    annotations = {
+      "kubernetes.io/service-account.name" = google_service_account_key.k8s_spark.name
+    }
+  }
+  data = {
+    "key.json" = base64decode(google_service_account_key.k8s_spark.private_key)
+  }
+  type = "kubernetes.io/service-account-token"
 }
