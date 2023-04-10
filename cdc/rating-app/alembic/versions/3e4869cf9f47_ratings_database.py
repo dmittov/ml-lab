64aa764ce324c219bf06e128624bfa654afd9497
Dmitry Mittov <mittov@gmail.com>
2023-04-10 20:39:08 +0200

    ratings app

---
+++ cdc/rating-app/alembic/versions/3e4869cf9f47_ratings_database.py
@@ -0,0 +1,24 @@
+"""Ratings database
+
+Revision ID: 3e4869cf9f47
+Revises: 
+Create Date: 2023-04-10 20:38:09.241312
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3e4869cf9f47'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ cdc/rating-app/domain/ratings.py
@@ -0,0 +1,40 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.sql import func
+import datetime
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+    user_id = Column(Integer, primary_key=True, index=True)
+    # keep it simple, no first/last names, it's not a real app
+    user_name = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_dt = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class Artist(Base):
+    __tablename__ = "artist"
+    artist_id = Column(Integer, primary_key=True, index=True)
+    artist_name = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_dt = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class Track(Base):
+    __tablename__ = "track"
+    track_id = Column(Integer, primary_key=True, index=True)
+    artist_id = Column(Integer, ForeignKey(Artist.artist_id))
+    track_name = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_dt = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class Rating(Base):
+    __tablename__ = "rating"
+    track_id = Column(Integer, ForeignKey(Track.track_id))
+    user_id = Column(Integer, ForeignKey(User.user_id))
+    stars = Column(Integer, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
