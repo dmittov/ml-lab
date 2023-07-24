ac60604c11f8cfaaea491f198daec34d5f1bc495
Dmitry Mittov <mittov@gmail.com>
2023-07-24 16:19:32 +0200

    new buckets

--- terraform/persistent/main.tf
+++ terraform/persistent/main.tf
@@ -6,7 +6,23 @@ resource "google_service_account" "default" {
 }
 
 resource "google_storage_bucket" "datasets" {
-  name                        = "${var.project}-datasets"
+  name                        = "${data.google_client_config.default.project}-datasets"
+  location                    = data.google_client_config.default.region
+  project                     = data.google_client_config.default.project
+  force_destroy               = false
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "postgres" {
+  name                        = "${data.google_client_config.default.project}-postgres"
+  location                    = data.google_client_config.default.region
+  project                     = data.google_client_config.default.project
+  force_destroy               = false
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "mlflow" {
+  name                        = "${data.google_client_config.default.project}-mlflow"
   location                    = data.google_client_config.default.region
   project                     = data.google_client_config.default.project
   force_destroy               = false
@@ -14,7 +30,7 @@ resource "google_storage_bucket" "datasets" {
 }
 
 resource "google_storage_bucket" "tf-state" {
-  name                        = "${var.project}-tf-state"
+  name                        = "${data.google_client_config.default.project}-tf-state"
   location                    = data.google_client_config.default.region
   project                     = data.google_client_config.default.project
   force_destroy               = false
@@ -25,9 +41,16 @@ resource "google_storage_bucket" "tf-state" {
 }
 
 resource "google_storage_bucket" "spark" {
-  name                        = "${var.project}-spark"
+  name                        = "${data.google_client_config.default.project}-spark"
   location                    = data.google_client_config.default.region
   project                     = data.google_client_config.default.project
   force_destroy               = false
   uniform_bucket_level_access = true
+}
+
+resource "google_artifact_registry_repository" "docker-main" {
+  location      = data.google_client_config.default.region
+  repository_id = "main"
+  description   = "main docker repository"
+  format        = "DOCKER"
 }
