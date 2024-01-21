ab6c0574156bb893ace075bc00ef9b86b137a3d4
Dmitry Mittov <mittov@gmail.com>
2024-01-21 12:40:29 +0100

    pub/sub subscriptions

--- terraform/vertex/pubsub.tf
+++ terraform/vertex/pubsub.tf
@@ -4,7 +4,14 @@ resource "google_pubsub_schema" "house_features" {
   definition = file("${path.module}/../../vertex/proto/house_features.proto")
 }
 
-resource "google_pubsub_topic" "house_pricing" {
+resource "google_pubsub_schema" "house_prices" {
+  name       = "house_features"
+  type       = "PROTOCOL_BUFFER"
+  definition = file("${path.module}/../../vertex/proto/house_prices.proto")
+}
+
+
+resource "google_pubsub_topic" "house_features" {
   name       = "house_prices"
   depends_on = [google_pubsub_schema.house_features]
 
@@ -14,12 +21,38 @@ resource "google_pubsub_topic" "house_pricing" {
   }
 }
 
-resource "google_pubsub_subscription" "house_pricing_bigquery" {
-  name  = "house_prices_bigquery"
-  topic = google_pubsub_topic.house_pricing.name
+resource "google_pubsub_topic" "house_prices" {
+  name       = "house_prices"
+  depends_on = [google_pubsub_schema.house_features]
+
+  schema_settings {
+    schema   = "projects/${google_bigquery_table.house_features.project}/schemas/house_prices"
+    encoding = "BINARY"
+  }
+}
+
+resource "google_pubsub_subscription" "house_features_bigquery" {
+  name  = "house_features_bigquery"
+  topic = google_pubsub_topic.house_features.name
 
   bigquery_config {
     table               = "${google_bigquery_table.house_features.project}.${google_bigquery_table.house_features.dataset_id}.${google_bigquery_table.house_features.table_id}"
+    use_topic_schema    = true
+    drop_unknown_fields = true
+    write_metadata      = true
+  }
+
+  depends_on = [
+    google_project_iam_member.viewer, google_project_iam_member.editor
+  ]
+}
+
+resource "google_pubsub_subscription" "house_prices_bigquery" {
+  name  = "house_prices_bigquery"
+  topic = google_pubsub_topic.house_features.name
+
+  bigquery_config {
+    table               = "${google_bigquery_table.house_features.project}.${google_bigquery_table.house_features.dataset_id}.${google_bigquery_table.house_prices.table_id}"
     use_topic_schema    = true
     drop_unknown_fields = true
     write_metadata      = true
