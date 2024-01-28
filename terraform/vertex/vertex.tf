608df484c6ebc082d89fe27c817ff5197fd0250d
Dmitry Mittov <mittov@gmail.com>
2024-01-28 13:55:05 +0100

    explicit entity_ids

--- terraform/vertex/vertex.tf
+++ terraform/vertex/vertex.tf
@@ -5,6 +5,7 @@ resource "google_vertex_ai_feature_group" "house_area" {
     big_query_source {
         input_uri = "bq://${google_bigquery_table.ai_house_area.project}.${google_bigquery_table.ai_house_area.dataset_id}.${google_bigquery_table.ai_house_area.table_id}"
     }
+    entity_id_columns = ["entity_id"]
   }
 }
 
@@ -15,6 +16,7 @@ resource "google_vertex_ai_feature_group" "house_price" {
     big_query_source {
         input_uri = "bq://${google_bigquery_table.ai_house_prices.project}.${google_bigquery_table.ai_house_prices.dataset_id}.${google_bigquery_table.ai_house_prices.table_id}"
     }
+    entity_id_columns = ["entity_id"]
   }
 }
 
@@ -26,28 +28,28 @@ resource "google_vertex_ai_feature_group" "house_price" {
 
 ### Online Serving
 
-# resource "google_vertex_ai_feature_online_store" "house_pricing" {
-#   name = "house_pricing"
-#   # optimized serving is already in preview and it's declared to have
-#   # much better response time, but it requires big_query / online_store sync
-#   # that consumes biqquery resources and costs money, very often syncs are not
-#   # welcomed
-#   #
-#   # overall: bigtable: worse latency, better costs
-#   bigtable {
-#     auto_scaling {
-#       min_node_count         = 1
-#       max_node_count         = 3
-#       cpu_utilization_target = 80
-#     }
+resource "google_vertex_ai_feature_online_store" "house_pricing" {
+  name = "house_pricing"
+  # optimized serving is already in preview and it's declared to have
+  # much better response time, but it requires big_query / online_store sync
+  # that consumes biqquery resources and costs money, very often syncs are not
+  # welcomed
+  #
+  # overall: bigtable: worse latency, better costs
+  bigtable {
+    auto_scaling {
+      min_node_count         = 1
+      max_node_count         = 3
+      cpu_utilization_target = 80
+    }
+  }
+#  attribute is not in production yet
+#  
+#   embedding_management {
+#     enabled = false  # out of scope for now
 #   }
-# #  attribute is not in production yet
-# #  
-# #   embedding_management {
-# #     enabled = false  # out of scope for now
-# #   }
-#   force_destroy = true
-# }
+  force_destroy = true
+}
 
 # resource "google_vertex_ai_feature_online_store_featureview" "featureview" {
 #   name                 = "example_feature_view"
